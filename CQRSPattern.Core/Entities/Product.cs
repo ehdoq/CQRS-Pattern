36cc35bd110d8c508ee9a14557d2d901c1282989
AloKzMstf <mstf.gdik19@gmail.com>
2022-10-18 13:18:08 +0300

    Core, Repository katmanı eklendi. Referanslar verildi. Paketler yüklendi. Entities eklendi.

---
+++ CQRSPattern.Core/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace CQRSPattern.Core.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
