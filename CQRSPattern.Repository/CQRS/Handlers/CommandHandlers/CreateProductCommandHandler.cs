8c48715eeef7dbba089ba3f0c839b25320dc1082
AloKzMstf <mstf.gdik19@gmail.com>
2022-10-18 13:57:58 +0300

    CQRS CommandHandlers eklendi.

---
+++ CQRSPattern.Repository/CQRS/Handlers/CommandHandlers/CreateProductCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using CQRSPattern.Core.Entities;
+using CQRSPattern.Repository.AppDBContext;
+using CQRSPattern.Repository.CQRS.Command.Request;
+using CQRSPattern.Repository.CQRS.Commands.Response;
+using MediatR;
+
+namespace CQRSPattern.Repository.CQRS.Handlers.CommandHandlers
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommandRequest, CreateProductCommandResponse>
+    {
+        private readonly AppDbContext _context;
+
+        public CreateProductCommandHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<CreateProductCommandResponse> Handle(CreateProductCommandRequest request, CancellationToken cancellationToken)
+        {
+            var id = Guid.NewGuid();
+
+            _context.Products.Add(new Product()
+            {
+                Id = id,
+                Name = request.Name,
+                Price = request.Price,
+                Quantity = request.Quantity,
+                CreateTime = DateTime.Now,
+            });
+
+            await _context.SaveChangesAsync();
+
+            return new CreateProductCommandResponse()
+            {
+                IsSuccess = true,
+                ProductId = id,
+            };
+        }
+    }
+}
---
+++ CQRSPattern.Repository/CQRS/Handlers/CommandHandlers/DeleteProductCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using CQRSPattern.Repository.AppDBContext;
+using CQRSPattern.Repository.CQRS.Command.Request;
+using CQRSPattern.Repository.CQRS.Commands.Response;
+using MediatR;
+
+namespace CQRSPattern.Repository.CQRS.Handlers.CommandHandlers
+{
+    public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommandRequest, DeleteProductCommandResponse>
+    {
+        private readonly AppDbContext _context;
+
+        public DeleteProductCommandHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<DeleteProductCommandResponse> Handle(DeleteProductCommandRequest request, CancellationToken cancellationToken)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Id == request.Id);
+
+            _context.Remove(product);
+
+            await _context.SaveChangesAsync();
+
+            return new DeleteProductCommandResponse()
+            {
+                IsSuccess = true
+            };
+        }
+    }
+}
