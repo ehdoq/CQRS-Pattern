6a9f2d04dbb42be3bdf24f9627a779455c6a4337
AloKzMstf <mstf.gdik19@gmail.com>
2022-10-18 14:15:40 +0300

    Eklemeler yapıldı.

--- CQRSPattern.API/Program.cs
+++ CQRSPattern.API/Program.cs
@@ -1,8 +1,16 @@
+using CQRSPattern.Repository.AppDBContext;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlite(builder.Configuration.GetConnectionString("sqliteCon")));
+builder.Services.AddMediatR(typeof(AppDbContext));
+
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,8 +20,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "CQRSPattern v1"));
 }
 
 app.UseHttpsRedirection();
