c3415db3acbd98c357774cb87997d9c4abdde802
AloKzMstf <mstf.gdik19@gmail.com>
2022-10-18 13:19:52 +0300

    AppDbContext eklendi.

---
+++ CQRSPattern.Repository/AppDBContext/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CQRSPattern.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace CQRSPattern.Repository.AppDBContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public IDbConnection Connection => Database.GetDbConnection();
+
+        public DbSet<Product>? Products { get; set; }
+    }
+}
