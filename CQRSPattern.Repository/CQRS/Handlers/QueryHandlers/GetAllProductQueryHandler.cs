b93ee813c652550cc6afc8c61e4a9ee6ba737472
AloKzMstf <mstf.gdik19@gmail.com>
2022-10-18 14:05:24 +0300

    CQRS QueryHandlers eklendi.

---
+++ CQRSPattern.Repository/CQRS/Handlers/QueryHandlers/GetAllProductQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using CQRSPattern.Repository.AppDBContext;
+using CQRSPattern.Repository.CQRS.Queries.Request;
+using CQRSPattern.Repository.CQRS.Queries.Response;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRSPattern.Repository.CQRS.Handlers.QueryHandlers
+{
+    public class GetAllProductQueryHandler : IRequestHandler<GetAllProductQueryRequest, List<GetAllProductQueryResponse>>
+    {
+        private readonly AppDbContext _context;
+
+        public GetAllProductQueryHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<GetAllProductQueryResponse>> Handle(GetAllProductQueryRequest request, CancellationToken cancellationToken)
+        {
+            return await _context.Products.Select(product => new GetAllProductQueryResponse()
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                Quantity = product.Quantity,
+                CreateTime = product.CreateTime
+            }).ToListAsync();
+        }
+    }
+}
---
+++ CQRSPattern.Repository/CQRS/Handlers/QueryHandlers/GetByIdProductQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using CQRSPattern.Repository.AppDBContext;
+using CQRSPattern.Repository.CQRS.Queries.Request;
+using CQRSPattern.Repository.CQRS.Queries.Response;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRSPattern.Repository.CQRS.Handlers.QueryHandlers
+{
+    public class GetByIdProductQueryHandler : IRequestHandler<GetByIdProductQueryRequest, GetByIdProductQueryResponse>
+    {
+        private readonly AppDbContext _context;
+
+        public GetByIdProductQueryHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GetByIdProductQueryResponse> Handle(GetByIdProductQueryRequest request, CancellationToken cancellationToken)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            return new GetByIdProductQueryResponse()
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                Quantity = product.Quantity,
+                CreateTime = product.CreateTime
+            };
+        }
+    }
+}
