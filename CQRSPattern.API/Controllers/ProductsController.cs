56cccac0bdcc4850e2dd7758add8e294e43ddef3
AloKzMstf <mstf.gdik19@gmail.com>
2022-10-18 14:32:57 +0300

    Son.

---
+++ CQRSPattern.API/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿using CQRSPattern.Repository.CQRS.Command.Request;
+using CQRSPattern.Repository.CQRS.Commands.Response;
+using CQRSPattern.Repository.CQRS.Queries.Request;
+using CQRSPattern.Repository.CQRS.Queries.Response;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRSPattern.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            List<GetAllProductQueryResponse> allProducts = await _mediator.Send(new GetAllProductQueryRequest());
+            return Ok(allProducts);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            GetByIdProductQueryResponse product = await _mediator.Send(new GetByIdProductQueryRequest() { Id = id });
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateProductCommandRequest requestModel)
+        {
+            CreateProductCommandResponse response = await _mediator.Send(requestModel);
+            return Ok(response);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            DeleteProductCommandResponse response = await _mediator.Send(new DeleteProductCommandRequest() { Id = id });
+            return Ok(response);
+        }
+    }
+}
